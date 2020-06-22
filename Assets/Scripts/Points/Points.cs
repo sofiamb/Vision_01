03eacc99c4b9e81bacad653974a58da9397726b2
sofia_b <sofiambombelli@gmail.com>
2020-06-22 03:32:08 -0300

    Showing points at lapse time. Show and Hide. Colored Result (yellow, Blue)

--- Assets/Scripts/Points/Points.cs
+++ Assets/Scripts/Points/Points.cs
@@ -1,39 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Points : MonoBehaviour
 {
     public Color Seen;
     public Color Missed;
-   
+    public bool itWasSeen = false;
+    
+    
+    public float SecondsToSeen = 2;
+    public TargetController targetController;
     void Start()
-    {
-        
+    {     
+        targetController = GameObject.FindGameObjectWithTag("VisionController").GetComponent<TargetController>();       
     }
 
-   
     void Update()
     {
+        if (isEnter == true)
+        {
+            if (timer > 2)
+            {
+                PointSeen();
+            }
+        }
         
+
+        timerToPoints();
+    }
+    public float timer = 0.0f;
+    public bool isEnter = false;
+    private void timerToPoints()
+    {
+        if (isEnter)
+        {
+            timer += Time.deltaTime; //Time.deltaTime will increase the value with 1 every second.
+            targetController.TimeText.text = "Seen Time = " + timer;
+        }
+    }
+    public void exitToPoint() {
+        timer = 0.0f;
+        isEnter = false;
     }
 
-    public void ActivatePoint() {
+    
+    public void enterToPoint() {
+        timer = 0.0f;
+        isEnter = true;        
+    }
+
+    public void ActivatePoint()
+    {
         this.gameObject.SetActive(true);
     }
 
-    public void timeToBeSeen() {
-        StartCoroutine(timeToSee());
-        
+    public void PointSeen() { 
+        this.changeColor(Seen);
+        itWasSeen = true;
     }
-    IEnumerator timeToSee() {
-        yield return new WaitForSeconds(2);
-        PointSeen();
-
-
-    }
-    public void PointSeen() { changeColor(Seen); }
     public void PointMissed() { changeColor(Missed);}
+    public void PointReset() { changeColor(new Color(255,255,255)); }
 
 
     private void changeColor(Color Col) {
--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -1,45 +1,123 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TargetController : MonoBehaviour
 {
 
     public GameObject PointPrefab;
-    private Vector3 pivot = new Vector3(0, 0, -1);
-    public int PointsNeeded;
-    Vector3 newPosition;
 
+    public GameObject[] Areas;
+    public List<Points> PointsNeeded = new List<Points>();
+    
+    public Text TimeText;
+    public Text ActualTimeText;
+    
+    public float TimeLapseTimer = 0.0f;
+
+    //---Appereance behaviour
+    public float TimeLapse;
+    public int pointToSeePerTime;
+    public float firstTimeToShow = 3;
+    public int ciclesCounter = 1;
+
+//    private int lastAreaScanned;
+
+    private Vector3 pivot = new Vector3(0, 0, -1);
+    
+   
+    
     // Spawn Are Radius (set by Scale)
     private float SpawnRadius = 10f;
     void Start()
     {
-      
-      
+        SetPointsList();
     }
 
-
-    //------- not needed ----
-    private void spwanAllPointsNeeded() {
-
-        for (int i = 0; i < PointsNeeded; i++) {
-          //  if (i < 8)
-           // {
-                setPoint(newPosition);
-                Debug.Log("point_"+i);
-            //}                
-     }
-    }
-
-    private void setPoint(Vector3 newPos)
+    void Update()
     {
-        GameObject go = Instantiate(PointPrefab);
         
-        go.transform.SetParent(this.transform);
-        go.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-        // go.transform.localPosition = new Vector3(0, 0, -1);
-      //  go.transform.localPosition = GetPosition();
+        TimeLapseToAppear();
+        
+    }
+
+   
+
+    void SetPointsList() {
+        Transform[] ParentToScan;
+
+        for (int i = 0; i < Areas.Length; i++)
+        {
+            ParentToScan = Areas[i].GetComponentsInChildren<Transform>();
+            int e = 0;
+            foreach (Transform go in ParentToScan)
+            {
+               if (e != 0)
+               {
+                  PointsNeeded.Add(go.GetComponent<Points>());
+                   go.gameObject.SetActive(false);
+               }
+                e++;
+            }
+        }
+    
+    }
+
+   
+  
+    //--Set time to appear
+    private void TimeLapseToAppear() {
+      
+        TimeLapseTimer += Time.deltaTime;
+        ActualTimeText.text = "Actual Time: " + TimeLapseTimer;
+
+        //First Appear 
+        if (TimeLapseTimer > firstTimeToShow) {//Seconds before start exam
+
+            //Start To Show at n time            
+            if (TimeLapseTimer >= TimeLapse * ciclesCounter)
+            {
+                if (ciclesCounter - 2 >= 0)
+                {
+                    if (PointsNeeded[ciclesCounter - 2].itWasSeen == false)
+                        PointsNeeded[ciclesCounter - 2].PointMissed();
+
+                    PointsNeeded[ciclesCounter - 2].gameObject.SetActive(false);
+
+                }
+                if (ciclesCounter < PointsNeeded.Count)
+                {
+                    PointsNeeded[ciclesCounter - 1].gameObject.SetActive(true);
+                    ciclesCounter++;
+                }
+
+            }
+            if (TimeLapseTimer >= TimeLapse * PointsNeeded.Count) {
+                Debug.Log("Finish!!");
+                for (int i = 0; i < PointsNeeded.Count; i++) {
+                    PointsNeeded[i].gameObject.SetActive(true);
+                }
+            }
+
+        }
+
+        
+        
 
     }
-  
+
+
+    private IEnumerator ShowAndHide(GameObject go, float time) {
+        go.SetActive(true);
+        yield return new WaitForSeconds(time);
+        go.SetActive(false);
+
+    }
+
+
+    //--Active elements by order
+
+
+
+   
 }
